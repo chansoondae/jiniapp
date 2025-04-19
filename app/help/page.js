d9a382e2e1bbb8092e0b8df6cac79467284faf90
chansoondae <info@mongtravel.net>
2025-04-19 19:51:06 +0900

    error fixed

--- app/help/page.js
+++ app/help/page.js
@@ -1,7 +1,7 @@
 'use client';
 
-import Header from '../components/common/Header';
-import BottomNav from '../components/common/BottomNav';
+import Header from '../components/Header';
+import BottomNav from '../components/BottomNav';
 
 export default function HelpPage() {
   return (
